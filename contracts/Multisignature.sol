456c3429b66b0c29a3fff80fb92048e964308db0
Saurabh Rana <saurabhsinghrana70@gmail.com>
2023-03-01 18:33:41 +0530

    The constructor now takes cofounder details and removed the unnecessary transaction details

--- contracts/Multisignature.sol
+++ contracts/Multisignature.sol
@@ -4,63 +4,74 @@ pragma solidity ^0.8.8;
 contract MultipartyPayments {
     
 string public orgName;
-address public orgWallet;
 uint stakesInFavour = 0;
 uint requiredMajority;
 
+struct coFounder {
+    string name;
+    uint stakes;
+}
+
 
 mapping (address => bool) public stakeHolderVoted;
-
 mapping (address => shareHolder) internal addressToStakeholder; // maps the address to the stakeholder
+mapping (address => coFounder) public addressToCoFounder;
+
+
 event NewStakeHolder (string name, uint stake); // notify when a new stakeholder is added
 event PaymentInitiated (address stakeAddress, uint amount); // notify when a payment is initialised
-
 event PaymentReceived (address senderAddress, uint amountSent);
 event PaymentSuccessful (uint amount, address payable sendTo);
 event PaymentUnsuccessful ();
 
 shareHolder[] private inFavour;
-paymentDetails[] private allTransactions;
+coFounder[] public coFounders;
+coFounder[] private inFavourCoFounders;
 
 struct shareHolder { // shareholders with the respective stakes
     string name;
     uint stakes;
 }
 
-struct paymentDetails { // detials regarding a payment
-    uint amount;
-    address initiatedBy;
-    address payable sendToAddress;
-    bool status;
+constructor (string memory _name, uint _requiredMajority, coFounder [] memory _coFounders, address [] memory _addressOfCoFounder) { // sets the organization name and majority mark along with the wallet address
+
+orgName = _name;
+requiredMajority = _requiredMajority;
+
+for (uint i = 0; i < _coFounders.length; i++){
+    coFounders.push(_coFounders[i]);
+    }
+
+for (uint i = 0; i < _coFounders.length; i++){
+    addressToCoFounder[_addressOfCoFounder[i]] = coFounders[i];
+    }
+    
 }
 
-constructor (string memory _name, uint _requiredMajority, address payable _orgWallet) { // sets the organization name and majority mark along with the wallet address
+function addStakeHolder (string memory _name, uint _stake, address _stakeHolderAddress) public { // adds stakeholders
 
-    orgName = _name;
-    requiredMajority = _requiredMajority;
-    orgWallet = _orgWallet;
-}
-
-function addStakeHolder (string memory _name, uint _stake, address _stakeAddress) public { // adds stakeholders
-
-    require(msg.sender == orgWallet);
-    addressToStakeholder[_stakeAddress] = shareHolder(_name, _stake);
-    emit NewStakeHolder (_name, _stake);
+require(addressToCoFounder[msg.sender].stakes > 0);
+addressToStakeholder[_stakeHolderAddress] = shareHolder(_name, _stake);
+emit NewStakeHolder (_name, _stake);
 }
 
 
 function voteForPayment (bool _inFavour) public {
 
-require(addressToStakeholder[msg.sender].stakes > 0); // verifying that the person is a stakeHolder of the company 
+require(addressToStakeholder[msg.sender].stakes > 0 || addressToCoFounder[msg.sender].stakes > 0); // verifying that the person is a stakeHolder of the company 
 require(stakeHolderVoted[msg.sender] == false); // verifying that the stakeholder has not casted his vote
 if (_inFavour){
+
+    if (addressToStakeholder[msg.sender].stakes > 0){
         string memory _name = addressToStakeholder[msg.sender].name;
         uint _stake = addressToStakeholder[msg.sender].stakes;
         inFavour.push(shareHolder(_name, _stake));
-    
-    
-    for (uint i = 0; i < inFavour.length; i++){
-        stakesInFavour += inFavour[i].stakes;
+    }
+    else {
+        string memory _name = addressToCoFounder[msg.sender].name;
+        uint _stake = addressToCoFounder[msg.sender].stakes;
+        inFavourCoFounders.push(coFounder(_name, _stake));
+
     }
     stakeHolderVoted[msg.sender] = true; // mark the stakeHolder as voted
 }
@@ -69,13 +80,22 @@ else {
     stakeHolderVoted[msg.sender] = true;
 }
 
-
 }
+
+function evaluateResults () public {
+    for (uint i = 0; i < inFavour.length; i++){
+        stakesInFavour += inFavour[i].stakes;
+    }
+    for (uint j = 0; j < inFavourCoFounders.length; j++){
+        stakesInFavour += inFavourCoFounders[j].stakes;
+    }
+}
+
 
 function paymentToAddress (uint _amount, address payable _sendTo) public payable {
 
 emit PaymentInitiated (msg.sender, _amount);
-require(addressToStakeholder[msg.sender].stakes > 0);
+require(addressToStakeholder[msg.sender].stakes > 0 || addressToCoFounder[msg.sender].stakes > 0);
 
 require(address(this).balance >= _amount, "Insufficient balance for the transaction to occur.");
 
@@ -86,26 +106,10 @@ uint TransferAmount = _amount;
 
 emit PaymentSuccessful(_amount, _sendTo);
 
-paymentDetails memory Payment = paymentDetails(_amount, msg.sender, _sendTo, true);
-allTransactions.push(Payment);
-
 }
 
 else {
     emit PaymentUnsuccessful();
-}
-// stakesInFavour = 0;
-// for (uint i = 0; i < inFavour.length; i++){
-// inFavour.pop();
-// }
-}
-
-function transactionHistory () public view returns(paymentDetails [] memory){
-    return allTransactions;
-}
-
-receive() payable external {
-    emit PaymentReceived (msg.sender, msg.value);
-}
-
+        }
+    }
 }
