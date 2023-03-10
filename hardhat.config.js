18c0c553e8c7b2ab2ef694477b5809a355236dcd
Saurabh Rana <saurabhsinghrana70@gmail.com>
2023-03-10 21:54:45 +0530

    Written the deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
+require("hardhat-deploy");
+require("ethers")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
   solidity: "0.8.17",
-  defaultNetwork: "polygon_mumbai",   
+  defaultNetwork: "hardhat",   
   networks: {
     polygon_mumbai: {      
-    url: `${process.env.API_URL}`,
-    accounts: [`0x${process.env.PRIVATE_KEY}`],
+    url: "https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78",
+    accounts: [PRIVATE_KEY],
+    ChainId: 80001,
   },
-}
+  localhost: {
+    url: "http://127.0.0.1:8545/",
+    chainId: 31337,
+    // accounts are generated as we activate the hardhat node
+},
+  },
+  solidity: "0.8.8",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
